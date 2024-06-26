ff20291398e443a4e3c7cb44734e4ca61cccf399
ashraful2019 <49226001+ashraful2019@users.noreply.github.com>
2024-06-26 13:45:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 let countStr = 0 
+
 function increment() {
     
+    countEl.textContent = 0
     count += 1
     countEl.textContent = count
 }
 
-function save() {
-    countStr = count + " - "
+@@ -13,10 +14,13 @@ function save() {
     
     saveEl.textContent += countStr
     countEl.textContent = 0
+    countStr = 0
+    
 }
 
 function reset() {
     count = 0
     countStr = 0
+    saveEl.textContent = ""
 }
-
